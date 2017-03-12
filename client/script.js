45f75df82e5b8c5d35b58f81ba529102c3b14416
moonisahmed <moonisahmed@live.com>
2017-03-11 23:31:02 -0700

    New update :100:
    owuquhruiehpruhgiperuhiTHECOMMENTjfiutwhufhoicafiofbbhap

--- client/script.js
+++ client/script.js
@@ -1,12 +1,84 @@
 var socket = io();
 
+//$('#submit').click(function() {
+//        $("#nameDivs").css("display","none");
+        
+            
+//    });
+//$('form').submit(function () {
+//    var text = $('#name').val();
+//    socket.emit('name', text);
+//    $('#name').val('');
+//    return false;
+//});
+
 $('form').submit(function () {
-  var text = $('#message').val();
-  socket.emit('message', text);
-  $('#message').val('');
-  return false;
+    var text = $('#message').val();
+    var nickname = $('#name').val();
+    socket.emit('message', text, nickname);
+    $('#message').val('');
+    $('#name').val('');
+    return false;
 });
 
+/*
+//message
 socket.on('message', function (msg) {
-  $('<li>').text(msg).appendTo('#history');
+  //$('<li class="list-group-item">').text(msg).appendTo('#history');
+    $('#history').append($('<h4 class="list-group-item" id="currentUserMessage">').text(msg));
+    $('#currentUserMessage').css("font-weight","bold");
+    $('#currentUserMessage' ).css("background-color","yellow");
+});
+
+
+//timestamp
+socket.on('time', function(time) {
+  $('#history').append($('<p>').text(time));
+});
+
+
+
+//User
+socket.on('name', function(name) {   
+    $('#history').append($('<p id="currentUserHistory">').text("By: " + name));
+    $('#currentUserHistory').css("font-weight","bold");
+    $('#currentUserHistory' ).css("background-color","yellow");
+    
+});
+*/
+//CurrentUser
+socket.on('newUser', function(name,chatHist) {
+    $('#users').append($('<li class="list-group-item" class="currentUser">').text(name));
+    $('.currentUser' ).css("background-color","yellow");
+    $('.currentUser').css("font-weight","bold");
+    // $("#nameDivs").css("display","none");
+    
+    for(var chat in chatHist)
+    {
+        $('#history').append($('<h4 class="list-group-item" id="currentUserMessage">').text(chatHist[chat].message));
+        $('#history').append($('<p>').text(chatHist[chat].time));
+        $('#history').append($('<p class="currentUserHistory">').text("By: " + chatHist[chat].name));
+    }
+    
+    
+});
+
+
+
+socket.on('test', function(name,msg,time) {
+
+    $('#history').append($('<h4 class="list-group-item" id="currentUserMessage">').text(msg));
+    $('#currentUserMessage').css("font-weight","bold");
+    $('#currentUserMessage' ).css("background-color","yellow");
+    
+    $('#history').append($('<p>').text(time));
+    
+    
+    $('#history').append($('<p class="currentUserHistory">').text("By: " + name));
+    $('.currentUserHistory').css("font-weight","bold");
+    $('.currentUserHistory' ).css("background-color","yellow");
+    
+    console.log(time + "time");
+    console.log(msg + "message");
+    console.log(name + "name");
 });
--- server.js
+++ server.js
@@ -1,19 +1,75 @@
+
+
+var userList = ['x'];
+var chatHist = [];
+var anonCount = 0;
 var express = require('express');
 var app = express();
 
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('client'));
-
+//app.use(express.static('client'));
+app.use(express.static(__dirname + '/client'));
 var io = require('socket.io')(server);
 
+
 io.on('connection', function (socket) {
-  socket.on('message', function (msg) {
-    io.emit('message', msg);
-  });
+    console.log('a user connected');
+    
+    socket.on('message', function (name,msg,time) {
+        
+        chatHist.push({name:name,message:msg,time:time});
+        
+        //io.emit('message', msg);
+        console.log('message: '+ msg);
+
+        //time stuff
+        var dt = new Date();
+        var utcDate = dt.toUTCString();
+        //var timeStamp = Math.floor(Date.now()/1000);        
+        //console.log(utcDate);
+        //io.emit('time',utcDate);
+        
+        //Nickname stuff
+        if(name ==="")
+        {
+            name = "anon" + anonCount.toString();
+            anonCount++;
+        }
+        
+        if(!userList.includes(name.toLowerCase()))
+        {
+            userList.push(name.toLowerCase());
+            io.emit('newUser',name,chatHist);
+            console.log("New User entered:", name);
+        }
+        //var timeAndMessage = [utcDate,msg]; 
+        io.emit('test',name, msg, utcDate);
+        console.log('server test');
+    });
+    /*
+    socket.on('name', function (name) { 
+        
+       
+        
+        io.emit('name',name);
+    });
+    
+    */
+    socket.on('disconnect', function () {
+    console.log("User disconnected");
+    });
+    
+   
+    
+   
 });
+
 
 server.listen(8080, function() {
   console.log('Chat server running');
 });
+
+
+
